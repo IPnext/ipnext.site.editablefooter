fef7689fd2d97d893c760653bfff1d5e9b6862d3
zedr <zedr@zedr.com>
2011-11-24 17:51:00 +0100

    fixed setuptools error

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '0.1'
 setup(name='ipnext.site.editablefooter',
       version=version,
       description="Edit the footer TTW",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
